762a04ca68b6cddaa7bd8cac03922d376f940448
HUNKMark2062 <kmark2062@gmail.com>
2024-10-05 12:19:35 +0200

    first version of the quiz program (GUI)

---
+++ quiz/exoplanetsNasaProject_01/src/exoplanetsnasaproject_01/ExoplanetsNasaProject_01.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package exoplanetsnasaproject_01;
+
+/**
+ *
+ * @author Mark
+ */
+public class ExoplanetsNasaProject_01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
